fb81221d1ac5aced6d23cd6149036116e1a650ce
looooo <sppedflyer@gmail.com>
2018-01-15 18:44:08 +0100

    update build number

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,13 +9,16 @@ cmake .. \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX \
       -D3RDPARTY_DIR=$PREFIX \
-      # -D3RDPARTY_TK_DIR=$PREFIX \
-      # -D3RDPARTY_TCL_DIR=$PREFIX \
-      # -D3RDPARTY_TK_LIBRARY_DIR=$PREFIX/lib \
-      # -D3RDPARTY_TCL_LIBRARY_DIR=$PREFIX/lib \
+      -D3RDPARTY_TK_DIR=$PREFIX \
+      -D3RDPARTY_TCL_DIR=$PREFIX \
+      -D3RDPARTY_TK_INCLUDE_DIR=$PREFIX/include \
+      -D3RDPARTY_TCL_INCLUDE_DIR=$PREFIX/include \
+      -D3RDPARTY_TK_LIBRARY_DIR=$PREFIX/lib \
+      -D3RDPARTY_TCL_LIBRARY_DIR=$PREFIX/lib \
+      -D3RDPARTY_
       -DUSE_TBB=ON \
       -DUSE_FREEIMAGE=ON \
       -DCMAKE_BUILD_TYPE="Release" \
 
-make -j2 2>&1 | tee output.txt
-make -j2 install
+make -j${CPU_COUNT} 2>&1 | tee output.txt
+make -j${CPU_COUNT} install
