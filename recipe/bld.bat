f8cd5e46a736a1166350cd2062cb3846e2e1e2e3
lo <sppedflyer@gmail.com>
2018-09-23 09:15:37 +0200

    win: use tbb add tbb-devel-dependency

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,8 @@ cmake .. -G "Ninja" ^
     -DINSTALL_DIR_LAYOUT="Unix" ^
     -DBUILD_MODULE_Draw=OFF ^
     -D3RDPARTY_DIR:FILEPATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_BUILD_TYPE="Release"
+    -DCMAKE_BUILD_TYPE="Release" ^
+    -DUSE_TBB=ON
 
 if errorlevel 1 exit 1
 ninja install
