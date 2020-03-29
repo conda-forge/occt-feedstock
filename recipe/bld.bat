d10c4644e35840a2987307acc6404fa153280753
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2020-03-29 22:37:50 +0200

    Clean intermediate build files on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,3 +12,11 @@ if errorlevel 1 exit 1
 cmake --build build -- TKG3d TKBool TKXmlTObj TKStdL TKIVtk TKSTEP209 TKShHealing TKLCAF TKSTL TKFeat TKOffset TKGeomBase TKXCAF
 
 if errorlevel 1 exit 1
+FOR /f "tokens=2" %%R IN ('findstr "_COMPILER_ _STATIC_LIBRARY_" rules.ninja') DO (
+    ninja -t clean -r %%~R
+)
+
+if errorlevel 1 exit 1
+cmake --build build -- install
+
+if errorlevel 1 exit 1
