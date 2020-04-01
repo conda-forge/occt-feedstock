b3dd1bca21360565d124183d7075edcabade32cb
adam-urbanczyk <adam.jan.urbanczyk@gmail.com>
2020-04-01 09:17:03 +0200

    Cleanup and install location fix

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,10 @@
-dir
-rmdir /S /Q data
-rmdir /S /Q dox
-rmdir /S /Q samples
-df -h
-
+REM Remove /GL to make this build fit 10G
 set "CXXFLAGS=%CXXFLAGS:-GL=%"
 
 cmake -S . -B build  -G Ninja ^
-      -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX ^
-      -D CMAKE_PREFIX_PATH:FILEPATH=$PREFIX ^
-      -D 3RDPARTY_DIR:FILEPATH=$PREFIX ^
+      -D CMAKE_INSTALL_PREFIX:FILEPATH=%PREFIX% ^
+      -D CMAKE_PREFIX_PATH:FILEPATH=%PREFIX% ^
+      -D 3RDPARTY_DIR:FILEPATH=%PREFIX% ^
       -D BUILD_MODULE_Draw:BOOL=OFF ^
       -D USE_TBB:BOOL=ON ^
       -D CMAKE_BUILD_TYPE:STRING="Release" ^
@@ -19,6 +14,5 @@ cmake -S . -B build  -G Ninja ^
 if errorlevel 1 exit 1
 
 cmake --build build -- -v install
-df -h
-bash -c "find -iname \*.cxx.obj -exec ls -lh {} \; ;"
+
 if errorlevel 1 exit 1
