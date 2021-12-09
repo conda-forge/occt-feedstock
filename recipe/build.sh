0d1d02fede80d98e03ef140143a1c2fd382a6939
Bas Nijholt <bas@nijho.lt>
2021-12-09 11:03:53 +0000

    make it work for EGL, osmesa, qt

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,15 @@
+CMAKE_ARGS=()
+
+if [[ "$build_variant" == "egl" ]]; then
+      CMAKE_ARGS+=(
+            "-DEGL_INCLUDE_DIR:PATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/include"
+            "-DEGL_LIBRARY:FILEPATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libEGL.so.1"
+            "-DOPENGL_egl_LIBRARY:FILEPATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib/libEGL.so.1"
+            "-DEGL_opengl_LIBRARY:FILEPATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so"
+            "-DOPENGL_opengl_LIBRARY:FILEPATH=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so"
+      )
+fi
+
 cmake -S . -B build  -G Ninja \
       -D CMAKE_FIND_ROOT_PATH="$PREFIX;$BUILD_PREFIX/$HOST/sysroot" \
       -D CMAKE_INSTALL_PREFIX:FILEPATH=$PREFIX \
@@ -12,6 +24,7 @@ cmake -S . -B build  -G Ninja \
       -D 3RDPARTY_VTK_INCLUDE_DIR:FILEPATH=$PREFIX/include/vtk-9.1 \
       -D VTK_RENDERING_BACKEND:STRING="OpenGL2" \
       -D USE_FREEIMAGE:BOOL=ON \
-      -D USE_RAPIDJSON:BOOL=ON
+      -D USE_RAPIDJSON:BOOL=ON \
+      "${CMAKE_ARGS[@]}"
 
 cmake --build build -- install
